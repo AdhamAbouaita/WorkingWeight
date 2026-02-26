4790c882b1a8ec5401690152128dc7c06e65b79d
adhamabouaita <adhamabouaita@mail.com>
2026-02-26 17:39:41 -0500

    full restart

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    getData: () => ipcRenderer.invoke('data:get'),
-    saveData: (entry) => ipcRenderer.invoke('data:save', entry)
-});
